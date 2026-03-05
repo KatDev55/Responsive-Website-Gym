ea5925d7051d7c96df38886830095496c0196800
KatDev55 <ddshop97026564@gmail.com>
2026-03-05 10:03:16 +0700

    FirstGit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {NavbarMenu} from "../../mockData/data"
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
---
+++ src/mockData/data.js
@@ -0,0 +1,28 @@
+export const NavbarMenu = [
+    {
+        id:1,
+        title:"home",
+        path:"/"
+    },
+    {
+        id:2,
+        title:"Trainer",
+        path:"#"
+    },
+    {
+        id:3,
+        title:"Program",
+        path:"#"
+    },
+    {
+        id:4,
+        title:"Blog",
+        path:"#"
+    },
+    {
+        id:5,
+        title:"Pricing",
+        path:"#"
+    
+    }
+]
